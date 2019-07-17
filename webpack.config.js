51f621a7c74da8c29cb9b0fecaa8bed29eb23886
Joseph Morse <tamed.lionheart@gmail.com>
2019-07-17 17:44:52 -0400

    build: changed to use index.jsx as entry file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: './src/index'
   },
   output: {
     path: `${__dirname}/docs`,
