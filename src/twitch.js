8a4361d5c8315d7e0193e884f95b338623be24d0
Joseph Morse <tamed.lionheart@gmail.com>
2019-07-19 07:25:31 -0400

    chore(twitch-api): made twitch api helper more generic

--- src/twitch.js
+++ src/twitch.js
@@ -1,14 +1,7 @@
 import axios from 'axios';
 
-export const users = axios.create({
-  baseURL: 'https://api.twitch.tv/helix/users',
-  headers: {
-    'Client-ID': 'az2bq1wyeazt3n4f3eb009097pbjk4'
-  }
-});
-
-export const streams = axios.create({
-  baseURL: 'https://api.twitch.tv/helix/streams',
+export const TwitchAPI = axios.create({
+  baseURL: 'https://api.twitch.tv/helix/',
   headers: {
     'Client-ID': 'az2bq1wyeazt3n4f3eb009097pbjk4'
   }
